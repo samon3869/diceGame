eeffb1881fecc2922c86a0e0cfaa10fec4c22dc1
keon <power3869@gmail.com>
2022-07-18 21:01:36 +0900

    studying children props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Button from './Button.js';
 import Dice from './Dice.js';
 
 function App() {
 	return (
 		<div>
+			<div>
+				<Button text="던지기" />
+				<Button text="처음부터" />
+			</div>
 			<Dice color='blue' num ={2} />
 		</div>
 	);
