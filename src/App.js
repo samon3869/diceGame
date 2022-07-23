cf711aace49c038c059b1453eaaa88592f1aa89d
keon <power3869@gmail.com>
2022-07-23 15:40:53 +0900

    one button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import Board from './Board';
+import Button from './Button.js';
+import { useState } from 'react';
+
+
+function random(n) {
+	return Math.ceil(Math.random() * n);
+}
 
 function App() {
+	const [num, setNum] = useState(1);
+	const [sum, setSum] = useState(0);
+	const [gameHistory, setGameHistory] = useState([]);
+	const [otherNum, setOtherNum] = useState(1);
+	const [otherSum, setOtherSum] = useState(0);
+	const [otherGameHistory, setOtherGameHistory] = useState([]);
+
+	const handleRollClick = () => {
+		const nextNum = random(6);
+		const nextOtherNum = random(6);
+		setNum(nextNum);
+		setSum(sum + nextNum);
+		setGameHistory([...gameHistory, nextNum]);
+		setOtherNum(nextOtherNum);
+		setOtherSum(otherSum + nextOtherNum);
+		setOtherGameHistory([...otherGameHistory, nextOtherNum]);
+	};
+
+	const handleClearClick = () => {
+		setNum(1);
+		setSum(0);
+		setGameHistory([]);
+	};
+
 	return (
 		<div>
-			<Board name="나" color="blue" />
-			<Board name="상대" color="red" />
-		</div> 
-	)
+			<div>
+				<Button onClick={handleRollClick}>던지기</Button>
+				<Button onClick={handleClearClick}>처음부터</Button>
+			</div>
+			<div>
+				<Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory} />
+				<Board name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory} />
+			</div> 
+		</div>
+	);
 }
 
 export default App;
--- src/Board.js
+++ src/Board.js
@@ -1,43 +1,15 @@
-import { useState } from 'react';
-import Button from './Button.js';
 import Dice from './Dice.js';
 
-function random(n) {
-	return Math.ceil(Math.random() * n);
-}
-
-function Board({name, color}) {
-	const [num, setNum] = useState(1);
-	const [sum, setSum] = useState(0);
-	const [gameHistory, setGameHistory] = useState([]);
-
-	const handleRollClick = () => {
-		const nextNum = random(6);
-		setNum(nextNum);
-		setSum(sum + nextNum);
-		setGameHistory([...gameHistory, nextNum]);
-	};
-
-	const handleClearClick = () => {
-		setNum(1);
-		setSum(0);
-		setGameHistory([]);
-	};
+function Board({name, color, num, sum, gameHistory}) {
 
 	return (
 		<div>
-			<div>
-				<Button onClick={handleRollClick}>던지기</Button>
-				<Button onClick={handleClearClick}>처음부터</Button>
-			</div>
-			<div>
-				<h2>{name}</h2>
-				<Dice color={color} num ={num} />
-				<h2>총점</h2>
-				<p>{sum}</p>
-				<h2>기록</h2>
-				<p>{gameHistory.join(', ')}</p>
-			</div>
+			<h2>{name}</h2>
+			<Dice color={color} num ={num} />
+			<h2>총점</h2>
+			<p>{sum}</p>
+			<h2>기록</h2>
+			<p>{gameHistory.join(', ')}</p>
 		</div>
 	);
 }
