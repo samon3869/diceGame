ad1f6205fdc20bd8205d5af32ace8b51e3dbd4aa
keon <power3869@gmail.com>
2022-07-23 15:27:47 +0900

    divide component using Board

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
-import { useState } from 'react';
-import Button from './Button.js';
-import Dice from './Dice.js';
-
-function random(n) {
-	return Math.ceil(Math.random() * n);
-}
+import Board from './Board';
 
 function App() {
-	const [num, setNum] = useState(1);
-	const [sum, setSum] = useState(0);
-	const [gameHistory, setGameHistory] = useState([]);
-
-	const handleRollClick = () => {
-		const nextNum = random(6);
-		setNum(nextNum);
-		setSum(sum + nextNum);
-		setGameHistory([...gameHistory, nextNum]);
-	};
-
-	const handleClearClick = () => {
-		setNum(1);
-		setSum(0);
-		setGameHistory([]);
-	};
-
 	return (
 		<div>
-			<div>
-				<Button onClick={handleRollClick}>던지기</Button>
-				<Button onClick={handleClearClick}>처음부터</Button>
-			</div>
-			<div>
-				<h2>나</h2>
-				<Dice color='blue' num ={num} />
-				<h2>총점</h2>
-				<p>{sum}</p>
-				<h2>기록</h2>
-				<p>{gameHistory.join(', ')}</p>
-			</div>
-		</div>
-	);
+			<Board name="나" color="blue" />
+			<Board name="상대" color="red" />
+		</div> 
+	)
 }
 
 export default App;
