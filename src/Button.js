3d9a5f7ee91ca0fc511355d80bda2149b7595078
keon <power3869@gmail.com>
2022-07-23 16:51:54 +0900

    css className

--- src/Button.js
+++ src/Button.js
@@ -1,28 +1,12 @@
-const baseButtonStyle = {
-	padding: '14px 27px',
-	borderRadius: '30px',
-	outline: 'none',
-	cursor: 'pointer',
-	fontSize: '17px',
-};
+import './Button.css';
 
-const blueButtonStyle = {
-	...baseButtonStyle,
-	backgroundColor: 'rgba(0, 89, 225, 0.2)',
-	border: 'solid 1px #7090ff',
-	color: '#7090ff',
-};
-
-const redButtonStyle = {
-	...baseButtonStyle,
-	backgroundColor: 'rgba(225, 78, 78, 0.2)',
-	border: 'solid 1px #ff4664',
-	color: '#ff4664',
-};
-
-function Button({ children, onClick, color }) {
-	const style = color === 'red' ? redButtonStyle : blueButtonStyle;
-		return <button style={style} onClick={onClick}>{children}</button>;
+function Button({ children, onClick, color = 'blue', className = '' }) {
+	const classNames = `Button ${color} ${className}`;
+		return (
+			<button className={classNames} onClick={onClick}>
+				{children}
+			</button>
+		);
 }
 
 export default Button;
